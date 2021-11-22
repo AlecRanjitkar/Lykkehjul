e7de922716aad439b692f04d0a59b7335618c284
Alec <Alexranjitkar@gmail.com>
2021-11-22 14:06:49 +0100

    ændret listen af dyr til fodboldhold

--- app/src/main/java/com/example/lykkehjul/Data/Datasource.kt
+++ app/src/main/java/com/example/lykkehjul/Data/Datasource.kt
@@ -4,31 +4,33 @@ import com.example.lykkehjul.Model.Ord
 
 class Datasource {
 
+    // Liste af ord over Fodboldhold
     fun loadOrd(): List<Ord>{
         return listOf<Ord>(
-            Ord("tiger"),
-            Ord("falk"),
-            Ord("elefant"),
-            Ord("flodhest"),
-            Ord("dromedar"),
-            Ord("sommerfugl"),
-            Ord("krokodille"),
-            Ord("gepard"))
+            //Ord("real madrid"),
+            //Ord("chelsea"),
+            //Ord("barcelona"),
+            Ord("manchester city"),
+            //Ord("arsenal"),
+            //Ord("bayern munichen"),
+            //Ord("juventus"),
+            //Ord("ac milan")
+            //Ord("dortmund")
 
-
+             )
     }
 
     fun loadpoint(): List<Ord>{
         return listOf<Ord>(
             Ord("10"),
-            Ord("100"),
+            /*Ord("100"),
             Ord("500"),
             Ord("1000"),
             Ord("1500"),
             Ord("Ekstra liv"),
             Ord("Tabt liv"),
             Ord("Konkurs"),
-
+*/
 
         )
 
